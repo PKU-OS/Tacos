4926bba148990dc637498b4d3598c8e91646c8d5
Yuetian Wu <109222685+seajupiter@users.noreply.github.com>
2024-04-20 20:56:37 +0800

    fix a bug related to read_user_buf (#2)
    * fix a bug related to read_user_buf
    
    * Continue to remove the inline asm in write_user_byte
    
    * untrack .zed configuration file
    
    * small fix in pagefault handler
    
    ---------
    
    Co-authored-by: Yuetian Wu <wyt2357@outlook.com>

--- src/mem/userbuf.rs
+++ src/mem/userbuf.rs
@@ -1,6 +1,6 @@
 #![allow(dead_code)]
 
-use core::arch::{asm, global_asm};
+use core::arch::global_asm;
 
 use crate::error::OsError;
 use crate::mem::in_kernel_space;
@@ -16,15 +16,8 @@ fn read_user_byte(user_src: *const u8) -> Result<u8> {
         return Err(OsError::BadPtr);
     }
 
-    let mut ret_status: isize = 0;
-    let mut byte: u8;
-    unsafe {
-        asm! {
-            "call __knrl_read_usr_byte",
-            in("a0") user_src,  lateout("a0") byte,
-            inout("a1") ret_status
-        }
-    };
+    let byte: u8 = 0;
+    let ret_status: u8 = unsafe { __knrl_read_usr_byte(user_src, &byte as *const u8) };
 
     if ret_status == 0 {
         Ok(byte)
@@ -43,15 +36,7 @@ fn write_user_byte(user_src: *const u8, value: u8) -> Result<()> {
         return Err(OsError::BadPtr);
     }
 
-    let mut ret_status: isize = 0;
-    unsafe {
-        asm! {
-            "call __knrl_write_usr_byte",
-            in("a0") user_src,
-            inout("a1") ret_status,
-            in("a2") value
-        }
-    };
+    let ret_status: u8 = unsafe { __knrl_write_usr_byte(user_src, value) };
 
     if ret_status == 0 {
         Ok(())
@@ -61,9 +46,11 @@ fn write_user_byte(user_src: *const u8, value: u8) -> Result<()> {
 }
 
 extern "C" {
-    pub fn __knrl_read_usr_byte(user_src: *const u8);
+    pub fn __knrl_read_usr_byte(user_src: *const u8, byte_ptr: *const u8) -> u8;
+    pub fn __knrl_read_usr_byte_pc();
     pub fn __knrl_read_usr_exit();
-    pub fn __knrl_write_usr_byte(user_src: *const u8);
+    pub fn __knrl_write_usr_byte(user_src: *const u8, value: u8) -> u8;
+    pub fn __knrl_write_usr_byte_pc();
     pub fn __knrl_write_usr_exit();
 }
 
@@ -71,20 +58,30 @@ global_asm! {r#"
         .section .text
         .globl __knrl_read_usr_byte
         .globl __knrl_read_usr_exit
+        .globl __knrl_read_usr_byte_pc
 
     __knrl_read_usr_byte:
+        mv t1, a1
+        li a1, 0
+    __knrl_read_usr_byte_pc:
         lb t0, (a0)
     __knrl_read_usr_exit:
         # pagefault handler will set a1 if any error occurs
-        mv a0, t0
+        sb t0, (t1)
+        mv a0, a1
         ret
 
         .globl __knrl_write_usr_byte
         .globl __knrl_write_usr_exit
+        .globl __knrl_write_usr_byte_pc
 
     __knrl_write_usr_byte:
-        sb a2, (a0)
+        mv t1, a1
+        li a1, 0
+    __knrl_write_usr_byte_pc:
+        sb t1, (a0)
     __knrl_write_usr_exit:
         # pagefault handler will set a1 if any error occurs
+        mv a0, a1
         ret
 "#}
--- src/trap/pagefault.rs
+++ src/trap/pagefault.rs
@@ -1,5 +1,5 @@
 use crate::mem::userbuf::{
-    __knrl_read_usr_byte, __knrl_read_usr_exit, __knrl_write_usr_byte, __knrl_write_usr_exit,
+    __knrl_read_usr_byte_pc, __knrl_read_usr_exit, __knrl_write_usr_byte_pc, __knrl_write_usr_exit,
 };
 use crate::mem::PageTable;
 use crate::thread::{self};
@@ -40,11 +40,11 @@ pub fn handler(frame: &mut Frame, fault: Exception, addr: usize) {
 
     match privilege {
         SPP::Supervisor => {
-            if frame.sepc == __knrl_read_usr_byte as _ {
+            if frame.sepc == __knrl_read_usr_byte_pc as _ {
                 // Failed to read user byte from kernel space when trap in pagefault
                 frame.x[11] = 1; // set a1 to non-zero
                 frame.sepc = __knrl_read_usr_exit as _;
-            } else if frame.sepc == __knrl_write_usr_byte as _ {
+            } else if frame.sepc == __knrl_write_usr_byte_pc as _ {
                 // Failed to write user byte from kernel space when trap in pagefault
                 frame.x[11] = 1; // set a1 to non-zero
                 frame.sepc = __knrl_write_usr_exit as _;
