e7d3a7a828ae77e662e20db189356906e4a730b2
PengZijun <2200012909@stu.pku.edu.cn>
2025-02-24 11:56:01 +0800

    Reset test priority-fifo, last change cause some problem

--- test/schedule/priority/fifo.rs
+++ test/schedule/priority/fifo.rs
@@ -3,27 +3,15 @@ use alloc::vec::Vec;
 
 use super::pass;
 use crate::sbi::interrupt;
-use crate::sync::Condvar;
 use crate::thread::*;
 
 const THREAD_CNT: usize = 16;
 const ITERS: usize = 16;
 
-fn thread_func(
-    tid: usize,
-    v: Arc<Mutex<Vec<usize>>>,
-    cvar: Arc<Condvar>,
-    mutex: Arc<Mutex<usize>>,
-) {
+fn thread_func(tid: usize, v: Arc<Mutex<Vec<usize>>>) {
     interrupt::set(false);
 
-    let mut guard = mutex.lock();
-    *guard += 1;
-    while *guard != THREAD_CNT {
-        cvar.wait(&mut guard);
-    }
-    cvar.notify_all();
-    drop(guard);
+    // TODO: Sync all threads here, is it necessary?
 
     for _ in 0..ITERS {
         v.lock().push(tid);
@@ -35,15 +23,11 @@ pub fn main() {
     interrupt::set(false);
 
     let log = Arc::new(Mutex::new(Vec::new()));
-    let cvar = Arc::new(Condvar::new());
-    let mutex = Arc::new(Mutex::new(0));
     set_priority(PRI_DEFAULT + 2);
 
     for tid in 0..THREAD_CNT {
         let v = Arc::clone(&log);
-        let c = Arc::clone(&cvar);
-        let m = Arc::clone(&mutex);
-        Builder::new(move || thread_func(tid, v, c, m))
+        Builder::new(move || thread_func(tid, v))
             .name("simple thread")
             .priority(PRI_DEFAULT + 1)
             .spawn();
