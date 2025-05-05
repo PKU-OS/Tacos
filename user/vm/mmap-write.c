30c068bcfa0d346ecb6f92003b28166c6218a88d
Peng Zijun <2200012909@stu.pku.edu.cn>
2025-05-05 17:28:03 +0800

    Fix mmap-write

--- user/vm/mmap-write.c
+++ user/vm/mmap-write.c
@@ -8,18 +8,23 @@
 #define ACTUAL ((void*)0x10000000)
 
 void main() {
-    int fd;
+    int fd, i;
     mapid_t map;
     char buf[1024];
 
     /* Write file via mmap. */
-    assert((fd = open("sample.txt", O_TRUNC | O_RDWR)) > 2);
+    assert((fd = open("sample.txt", O_RDWR)) > 2);
     assert((map = mmap(fd, ACTUAL)) != MAP_FAILED);
-    memcpy(ACTUAL, sample, strlen(sample));
+
+    memset(ACTUAL, 42, strlen(sample));
     munmap(map);
 
     /* Read back via read(). */
     read(fd, buf, strlen(sample));
-    assert(!memcmp(buf, sample, strlen(sample)), "compare read data against written data");
+    for (i = 0; i < strlen(sample); i++)
+        assert(buf[i] == 42, "check that mmap write was successful");
+
+    /* Write origin content back */
+    write(fd, sample, strlen(sample));
     close(fd);
 }
